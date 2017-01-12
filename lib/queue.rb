572a5699c0277b0e73f4c3b00a01b7c78d70b391
lshapz <laurashapiro01@gmail.com>
2017-01-12 14:03:58 -0500

    Done.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,19 @@
 require_relative 'stack'
 
 class Queue
+  attr_accessor :stack1, :stack2
+
+  def initialize 
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def enqueue(item)
+    @stack1.me.push(item)
+  end
+
+  def dequeue
+    @stack1.me.shift
+  end
 
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,21 @@
 class Stack
 
+attr_accessor :me
+
+def initialize 
+  @me = []
+end
+
+def push(item) 
+  @me.push(item)
+end
+
+def pop 
+  @me.pop
+end
+
+def size
+  @me.length
+end
+
 end
